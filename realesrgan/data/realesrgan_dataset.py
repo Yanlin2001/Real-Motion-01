b5fde534910a51ab5a3727e88d5d657aa50b3b51
LinziGosh <1643515720@qq.com>
2024-02-15 22:28:40 +0800

    6

--- realesrgan/data/realesrgan_dataset.py
+++ realesrgan/data/realesrgan_dataset.py
@@ -181,7 +181,7 @@ class RealESRGANDataset(data.Dataset):
             sinc_kernel = self.pulse_tensor
 
         # BGR to RGB, HWC to CHW, numpy to tensor
-        img_gt = img2tensor([img_gt], bgr2rgb=True, float32=True)[0]
+        img_gt = img2tensor([img_gt], bgr2rgb=False, float32=True)[0]
         kernel = torch.FloatTensor(kernel)
         kernel2 = torch.FloatTensor(kernel2)
 
