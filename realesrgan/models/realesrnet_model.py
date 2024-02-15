713f1e9576e0888c27755f8340ae1b636c30518e
LinziGosh <1643515720@qq.com>
2024-02-15 23:04:05 +0800

    6

--- realesrgan/models/realesrnet_model.py
+++ realesrgan/models/realesrnet_model.py
@@ -173,7 +173,10 @@ class RealESRNetModel(SRModel):
 
             print('gt shape:', self.gt.shape)
 
+            # 转为单通道灰度图
+            out = self.gt.mean(dim=1, keepdim=True)
 
+            print('out shape:', out.shape)
 
 
             # ----------------------- The first degradation process ----------------------- #
