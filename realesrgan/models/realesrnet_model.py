04d28d89b567a8c5a82f9b6cd4f8e430fc275235
LinziGosh <1643515720@qq.com>
2024-02-15 20:58:48 +0800

    0+

--- realesrgan/models/realesrnet_model.py
+++ realesrgan/models/realesrnet_model.py
@@ -8,7 +8,7 @@ from basicsr.utils import DiffJPEG, USMSharp
 from basicsr.utils.img_process_util import filter2D
 from basicsr.utils.registry import MODEL_REGISTRY
 from torch.nn import functional as F
-
+from torchvision import transforms
 
 @MODEL_REGISTRY.register()
 class RealESRNetModel(SRModel):
@@ -80,7 +80,96 @@ class RealESRNetModel(SRModel):
 
             ori_h, ori_w = self.gt.size()[2:4]
 
+            # ----------------------- The first motion process ----------------------- #
+            def random_motion_transform(image, width, height,
+                            rotate_prob = 0.5, rotate_range = [-5, 5],
+                            translation_prob = [0.2, 0.2, 0.6], translation_range = [-0.1, 0.1],
+                            perspective_prob = [0.3, 0.3, 0.4], perspective_range = [-0.1, 0.1],
+                            stretch_prob = 0.4, stretch_range = [-0.1, 0.1]):
+                # 旋转图像 rotate
+                if np.random.uniform(0, 1) < rotate_prob:
+                    rotate_angle = np.random.uniform(rotate_range[0], rotate_range[1])
+                    out = rotate(image, rotate_angle, interpolation=InterpolationMode.BILINEAR)
+                else:
+                    out = image
+
+                # 平移图像 translation
+                translation_type = np.random.choice(["left-right", "up-down", "keep"], p=translation_prob)
+                translation_rate = np.random.uniform(-translation_range[0], translation_range[0])
+                if translation_type == "left-right":
+                    out = out.roll(int(translation_rate * width), 2)
+                elif translation_type == "up-down":
+                    out = out.roll(int(translation_rate * height), 1)
+                else:
+                    out = out
+
+                # 透视变换 perspective
+                original_pts = [[0, 0], [width, 0], [0, height], [width, height]]
+                perspective_type = np.random.choice(["pitch", "yaw", "keep"], p=perspective_prob)
+                pitch_rate1 = np.random.uniform(-perspective_range[0], perspective_range[0])
+                pitch_rate2 = np.random.uniform(-perspective_range[0], perspective_range[0])
+                yaw_rate1 = np.random.uniform(-perspective_range[1], perspective_range[1])
+                yaw_rate2 = np.random.uniform(-perspective_range[1], perspective_range[1])
+                if np.random.uniform(0, 1) < stretch_prob:
+                    stretch_rate1 = np.random.uniform(stretch_range[0], stretch_range[1])
+                    stretch_rate2 = np.random.uniform(stretch_range[0], stretch_range[1])
+                else:
+                    stretch_rate1 = 0
+                    stretch_rate2 = 0
+                if perspective_type == "pitch":
+                    pitch_pts = [
+                        [0 - pitch_rate1 * width, 0 - stretch_rate1 * height],
+                        [width + pitch_rate1 * width, 0 - stretch_rate1 * height],
+                        [0 - pitch_rate2 * width, height + stretch_rate2 * height],
+                        [width + pitch_rate2 * width, height + stretch_rate2 * height],
+                    ]
+                    yaw_pts = original_pts
+                    out = perspective_transform(out, original_pts, pitch_pts)
+                elif perspective_type == "yaw":
+                    yaw_pts = [
+                        [0 - stretch_rate1 * width, 0 - yaw_rate1 * height],
+                        [width + stretch_rate2 * width, 0 - yaw_rate2 * height],
+                        [0 - stretch_rate1 * width, height + yaw_rate1 * height],
+                        [width + stretch_rate2 * width, height + yaw_rate2 * height],
+                    ]
+                    pitch_pts = original_pts
+                    out = perspective_transform(out, original_pts, yaw_pts)
+                else:
+                    pitch_pts = original_pts
+                    yaw_pts = original_pts
+                    stretch_pts = [
+                        [0 , 0 - stretch_rate1 * height],
+                        [width, 0 - stretch_rate1 * height],
+                        [0 , height + stretch_rate2 * height],
+                        [width, height + stretch_rate2 * height],
+                    ]
+                    out = perspective_transform(out, original_pts, stretch_pts)
+
+                return out
+
+            def kspace_scan(image_tensor, K_data, cur_round, tol_round):
+                # 将图像张量转换为K空间数据
+                k_space_data = np.fft.fft2(image_tensor, axes=(-2, -1))
+
+                # 进行 fftshift 操作将低频移到中心
+                k_space_data = np.fft.fftshift(k_space_data, axes=(-2, -1))
+
+                # 获取图像的高度和宽度
+                _, H, W = image_tensor.shape
+
+                # 计算当前应该填充的行范围
+                start_row = cur_round * H // tol_round
+                end_row = (cur_round + 1) * H // tol_round
+                print(start_row, end_row)
+
+                # 截取并填充到K_data中
+                K_data[:, start_row:end_row, :] = k_space_data[:, start_row:end_row, :]
+
+                return K_data
+
+            print('gt shape:', self.gt.shape)
             # ----------------------- The first degradation process ----------------------- #
+
             # blur
             out = filter2D(self.gt, self.kernel1)
             # random resize
@@ -105,75 +194,18 @@ class RealESRNetModel(SRModel):
                     gray_prob=gray_noise_prob,
                     clip=True,
                     rounds=False)
-            # JPEG compression
-            jpeg_p = out.new_zeros(out.size(0)).uniform_(*self.opt['jpeg_range'])
-            out = torch.clamp(out, 0, 1)  # clamp to [0, 1], otherwise JPEGer will result in unpleasant artifacts
-            out = self.jpeger(out, quality=jpeg_p)
-
-            # ----------------------- The second degradation process ----------------------- #
-            # blur
-            if np.random.uniform() < self.opt['second_blur_prob']:
-                out = filter2D(out, self.kernel2)
-            # random resize
-            updown_type = random.choices(['up', 'down', 'keep'], self.opt['resize_prob2'])[0]
-            if updown_type == 'up':
-                scale = np.random.uniform(1, self.opt['resize_range2'][1])
-            elif updown_type == 'down':
-                scale = np.random.uniform(self.opt['resize_range2'][0], 1)
-            else:
-                scale = 1
-            mode = random.choice(['area', 'bilinear', 'bicubic'])
-            out = F.interpolate(
-                out, size=(int(ori_h / self.opt['scale'] * scale), int(ori_w / self.opt['scale'] * scale)), mode=mode)
-            # add noise
-            gray_noise_prob = self.opt['gray_noise_prob2']
-            if np.random.uniform() < self.opt['gaussian_noise_prob2']:
-                out = random_add_gaussian_noise_pt(
-                    out, sigma_range=self.opt['noise_range2'], clip=True, rounds=False, gray_prob=gray_noise_prob)
-            else:
-                out = random_add_poisson_noise_pt(
-                    out,
-                    scale_range=self.opt['poisson_scale_range2'],
-                    gray_prob=gray_noise_prob,
-                    clip=True,
-                    rounds=False)
-
-            # JPEG compression + the final sinc filter
-            # We also need to resize images to desired sizes. We group [resize back + sinc filter] together
-            # as one operation.
-            # We consider two orders:
-            #   1. [resize back + sinc filter] + JPEG compression
-            #   2. JPEG compression + [resize back + sinc filter]
-            # Empirically, we find other combinations (sinc + JPEG + Resize) will introduce twisted lines.
-            if np.random.uniform() < 0.5:
-                # resize back + the final sinc filter
-                mode = random.choice(['area', 'bilinear', 'bicubic'])
-                out = F.interpolate(out, size=(ori_h // self.opt['scale'], ori_w // self.opt['scale']), mode=mode)
-                out = filter2D(out, self.sinc_kernel)
-                # JPEG compression
-                jpeg_p = out.new_zeros(out.size(0)).uniform_(*self.opt['jpeg_range2'])
-                out = torch.clamp(out, 0, 1)
-                out = self.jpeger(out, quality=jpeg_p)
-            else:
-                # JPEG compression
-                jpeg_p = out.new_zeros(out.size(0)).uniform_(*self.opt['jpeg_range2'])
-                out = torch.clamp(out, 0, 1)
-                out = self.jpeger(out, quality=jpeg_p)
-                # resize back + the final sinc filter
-                mode = random.choice(['area', 'bilinear', 'bicubic'])
-                out = F.interpolate(out, size=(ori_h // self.opt['scale'], ori_w // self.opt['scale']), mode=mode)
-                out = filter2D(out, self.sinc_kernel)
 
             # clamp and round
             self.lq = torch.clamp((out * 255.0).round(), 0, 255) / 255.
 
             # random crop
-            gt_size = self.opt['gt_size']
-            self.gt, self.lq = paired_random_crop(self.gt, self.lq, gt_size, self.opt['scale'])
+            #gt_size = self.opt['gt_size']
+            #self.gt, self.lq = paired_random_crop(self.gt, self.lq, gt_size, #self.opt['scale'])
 
             # training pair pool
             self._dequeue_and_enqueue()
             self.lq = self.lq.contiguous()  # for the warning: grad and param do not obey the gradient layout contract
+
         else:
             # for paired training or validation
             self.lq = data['lq'].to(self.device)
